4c27dd03c5918e27e447b47e24234dc1d53277e1
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2022-10-19 19:39:06 -0500

    feat: add container and text field

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { Container } from "@mui/material";
+import Searcher from "./components/Searcher";
 
 const App = () => {
-  return(
-    <div>Hola</div>
+  return (
+    <Container sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      background: 'whitesmoke',
+      width: '80vw',
+      height: '500px',
+      borderRadius: '16px',
+      marginTop: '50px'
+    }}>
+      <Searcher />
+    </Container>
   )
 };
 
---
+++ src/components/Searcher/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { IconButton, Stack, TextField } from "@mui/material";
+
+const Searcher = () => {
+  return (
+    <Stack>
+      <TextField>
+
+      </TextField>
+      <IconButton></IconButton>
+    </Stack>
+  )
+}
+
+export default Searcher;
