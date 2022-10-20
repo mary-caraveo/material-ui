2d78f46cf787fb2dede5596374275c5fbf5ec89e
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2022-10-20 16:11:48 -0500

    feat: connect the project to the GitHub API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container } from '@mui/material';
+import { getUser } from './services/users';
 import Searcher from './components/Searcher';
 
 const App = () => {
   const [inputUser, setInputUser] = useState('octocat');
   const [userState, setUserState] = useState('inputUser');
+
+  useEffect(() => {
+    const fetchUser = async (user) => {
+      const userResponse = await getUser(user);
+      console.log(userResponse);
+    };
+
+    fetchUser(inputUser);
+  }, []);
+
   return (
     <Container
       sx={{
