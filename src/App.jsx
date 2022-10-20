6d333d6f1b054f6e78ef3a867e9089d5e1687076
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2022-10-19 20:32:35 -0500

    feat: add styles to search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import React from "react";
-import { Container } from "@mui/material";
-import Searcher from "./components/Searcher";
+import React from 'react';
+import { Container } from '@mui/material';
+import Searcher from './components/Searcher';
 
 const App = () => {
   return (
-    <Container sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      background: 'whitesmoke',
-      width: '80vw',
-      height: '500px',
-      borderRadius: '16px',
-      marginTop: '50px'
-    }}>
+    <Container
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        background: 'rgba(144, 19, 254, 0.5)',
+        boxShadow: '0 8px 32px 0 rgba(31, 38, 135, 0.37)',
+        backdropFilter: 'blur(4px)',
+        border: '1px solid rgba(255, 255, 255, 0.18)',
+        width: '80vw',
+        height: '500px',
+        borderRadius: '16px',
+        marginTop: '50px',
+      }}>
       <Searcher />
     </Container>
-  )
+  );
 };
 
 export default App;
--- src/components/Searcher/index.jsx
+++ src/components/Searcher/index.jsx
@@ -1,15 +1,35 @@
-import React from "react";
-import { IconButton, Stack, TextField } from "@mui/material";
+import React from 'react';
+import { IconButton, Stack, TextField, InputAdornment } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Searcher = () => {
   return (
-    <Stack>
-      <TextField>
-
-      </TextField>
-      <IconButton></IconButton>
+    <Stack
+      direction="row"
+      sx={{
+        marginTop: '30px',
+        width: '80%',
+      }}>
+      <TextField
+        id="outlined-basic"
+        label="GitHub User"
+        placeholder="Octocat"
+        variant="outlined"
+        color="secondary"
+        size="small"
+        fullWidth={true}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="start">
+              <IconButton>
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
     </Stack>
-  )
-}
+  );
+};
 
 export default Searcher;
