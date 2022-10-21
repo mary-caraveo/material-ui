231d194f3e3e265483ebedf33ef8e578fc2333bf
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2022-10-21 17:45:05 -0500

    feat: add the user's main information

--- src/containers/UserCard/index.jsx
+++ src/containers/UserCard/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { CardMedia, Grid } from '@mui/material';
+import MainInformation from '../../components/MainInformation';
 
-const UserCard = ({ userState: { avatar_url } }) => {
+const UserCard = (props) => {
+  const { userState } = props;
+  const { avatar_url } = userState;
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={3}>
         <CardMedia component="img" alt="GitHub User" image={avatar_url} />
+      </Grid>
+      <Grid item xs={9}>
+        <MainInformation userState={userState} />
       </Grid>
     </Grid>
   );
