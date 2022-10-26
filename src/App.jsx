9238239da4af14471231235a76f70bebfdda2508
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2022-10-26 13:03:08 -0500

    feat: add custom theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Container } from '@mui/material';
+import { ThemeProvider } from '@mui/material';
 import { getUser } from './services/users';
 import Searcher from './components/Searcher';
 import UserCard from './containers/UserCard';
+import theme from '../src/theme';
 
 const App = () => {
   const [inputUser, setInputUser] = useState('octocat');
@@ -29,24 +31,27 @@ const App = () => {
   }, [inputUser]);
 
   return (
-    <Container
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        background: '#22272d',
-        boxShadow: '0 8px 32px 0 rgba(31, 38, 135, 0.37)',
-        backdropFilter: 'blur(4px)',
-        border: '1px solid rgba(255, 255, 255, 0.18)',
-        width: { xs: '100%', md: '80vw' },
-        height: { xs: '100%', md: '80vh', lg: '500px' },
-        borderRadius: '16px',
-        marginTop: { xs: '0', md: '30px', lg: '40px' },
-        paddingBottom: { xs: '1rem', sm: '3rem', lg: '0' },
-      }}>
-      <Searcher inputUser={inputUser} setInputUser={setInputUser} />
-      <UserCard userState={userState} />
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          background: '#22272d',
+          boxShadow: '0 8px 32px 0 rgba(31, 38, 135, 0.37)',
+          backdropFilter: 'blur(4px)',
+          border: '1px solid rgba(255, 255, 255, 0.18)',
+          width: { xs: '100%', md: '80vw' },
+          height: { xs: '100%', md: '80vh', lg: '500px' },
+          borderRadius: '16px',
+          marginTop: { xs: '0', md: '30px', lg: '40px' },
+          paddingBottom: { xs: '1rem', sm: '3rem', lg: '0' },
+        }}
+      >
+        <Searcher inputUser={inputUser} setInputUser={setInputUser} />
+        <UserCard userState={userState} />
+      </Container>
+    </ThemeProvider>
   );
 };
 
---
+++ src/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#009688',
+    },
+    secondary: {
+      main: '#0078AA',
+    },
+    tertiary: {
+      main: '#705772'
+    }
+  },
+});
+
+export default theme;
