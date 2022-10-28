9745405dda8364b94341deb7de7f1a37324cf7e4
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2022-10-27 22:21:16 -0500

    feat:  add background color and center user card with css

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { getUser } from './services/users';
 import Searcher from './components/Searcher';
 import UserCard from './containers/UserCard';
 import theme from '../src/theme';
+import './styles/globals.css';
 
 const App = () => {
   const [inputUser, setInputUser] = useState('octocat');
@@ -31,27 +32,29 @@ const App = () => {
   }, [inputUser]);
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          background: '#22272d',
-          boxShadow: '0 8px 32px 0 rgba(31, 38, 135, 0.37)',
-          backdropFilter: 'blur(4px)',
-          border: '1px solid rgba(255, 255, 255, 0.18)',
-          width: { xs: '100%', md: '80vw' },
-          height: { xs: '100%', md: '80vh', lg: '500px' },
-          borderRadius: '16px',
-          marginTop: { xs: '0', md: '30px', lg: '40px' },
-          paddingBottom: { xs: '1rem', sm: '3rem', lg: '0' },
-        }}
-      >
-        <Searcher inputUser={inputUser} setInputUser={setInputUser} />
-        <UserCard userState={userState} />
-      </Container>
-    </ThemeProvider>
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <Container
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            background: '#22272d',
+            boxShadow: '0 8px 32px 0 rgba(31, 38, 135, 0.37)',
+            backdropFilter: 'blur(4px)',
+            border: '1px solid rgba(255, 255, 255, 0.18)',
+            width: { xs: '100%', md: '80vw' },
+            height: { xs: '100%', md: '80vh', lg: '500px' },
+            borderRadius: '16px',
+            marginTop: { xs: '0', md: '30px', lg: '40px' },
+            paddingBottom: { xs: '1rem', sm: '3rem', lg: '0' },
+          }}
+        >
+          <Searcher inputUser={inputUser} setInputUser={setInputUser} />
+          <UserCard userState={userState} />
+        </Container>
+      </ThemeProvider>
+    </div>
   );
 };
 
